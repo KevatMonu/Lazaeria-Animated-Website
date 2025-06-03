e4488018d5fc67b98a9c683f43528176e9b129e1
Monu Kenvat <monukevat3@gmail.com>
2025-06-03 06:04:05 +0530

    new upadted

--- js/script.js
+++ js/script.js
@@ -61,4 +61,136 @@ function video() {
   });
 }
 
-video();
+
+function hover() {
+  // Select all pairs
+  let boxes = document.querySelectorAll(".page8-mini-box-con");
+  let hoverboxes = document.querySelectorAll(".shadow-mini-box");
+  let text = document.querySelectorAll(".page8-mini-box-con-left");
+  icon = document.querySelectorAll(".page8-mini-box-con-righthover");
+  icongo = document.querySelectorAll(".page8-mini-box-con-right");
+  
+  boxes.forEach((box, i) => {
+    let hoverbox = hoverboxes[i];
+    if (!hoverbox) return;
+
+    gsap.set(hoverbox, { 
+      top: "-100%", 
+      opacity: 0, 
+      display: "none" 
+    });
+    
+    box.addEventListener("mouseenter", () => {
+      gsap.killTweensOf(hoverbox); // Stop ongoing animations
+      gsap.set(hoverbox, { 
+        top: "-100%", 
+        display: "block", 
+        opacity: 0 
+      });
+      
+      gsap.to(box,{
+        borderTop: "2px solid #fff",
+      });
+      
+      gsap.to(hoverbox, {
+        top: "0",
+        height: "100%",
+        opacity: 1,
+        duration: 0.3,
+        ease: "power1.inOut",
+        zIndex: -1,
+      });
+      
+      gsap.to(text[i],{
+        paddingLeft:"1vw",
+        duration: 0.3,
+        ease: "power1.inOut",
+      })
+      
+      gsap.to(icon[i],{
+        display: "block",
+        opacity: 1,
+        duration: 0.5,
+        ease: "power1.inOut",
+      });
+      
+      
+      gsap.to(icongo[i],{
+        opacity: 0,
+        duration: 0.5,
+        ease: "power1.inOut",
+      })
+      
+      
+    });
+    
+    box.addEventListener("mouseleave", () => {
+      gsap.killTweensOf(hoverbox); // Stop ongoing animations
+      gsap.to(hoverbox, {
+        top: "100%",
+        height: "0",
+        opacity: 0,
+        duration: 0.4,
+        ease: "power1.inOut",
+        onComplete: () => {
+          gsap.set(hoverbox, { top: "-100%", display: "none" });
+        }
+      });
+      
+      gsap.to(text[i],{
+        paddingLeft:"0vw",
+        duration: 0.3,
+        ease: "power1.inOut",
+      })
+      
+      gsap.to(box,{
+        borderTop: " 2px solid #272727 ",
+        duration:0.5
+      });
+      
+      gsap.to(icon[i],{
+        display: "none",
+        opacity: 0,
+        duration: 0.5,
+        ease: "power1.inOut",
+      });
+      
+      gsap.to(icongo[i],{
+        opacity: 1,
+        duration: 0.5,
+        ease: "power1.inOut",
+      });
+      
+    });
+  });
+}
+
+function click() {
+
+let mainBox = document.querySelector('.page8-main-box');
+let box = document.querySelector('.page8-mini-box');
+let icon = document.querySelector('.rotateicon');
+
+
+
+mainBox.addEventListener('click', function () {
+  if (isHidden) {
+    box.style.display = 'block';
+    gsap.to(icon, {
+      rotation: 180,
+      duration: 0.3,
+      ease: "power1.inOut"
+    });
+  } else {
+    box.style.display = 'none';
+    gsap.to(icon, {
+      rotation: 0,
+      duration: 0.3,
+      ease: "power1.inOut"
+    });
+  }
+  
+});
+
+
+}
